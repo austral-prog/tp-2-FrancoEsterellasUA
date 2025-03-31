5de5607a1b549a94d5a72fcb0a118f343ee55cea
FrancoEsterellasUA <festerellas@mail.austral.edu.ar>
2025-03-31 11:00:43 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -3,4 +3,5 @@ def ada():
     last_name = "LoVeLAce"
     print(first_name.lower() , last_name.lower())
     print(first_name.capitalize() , last_name.capitalize())
+    print(first_name.upper(), last_name.upper())
     print('\t' , first_name.lower(), last_name.lower())
